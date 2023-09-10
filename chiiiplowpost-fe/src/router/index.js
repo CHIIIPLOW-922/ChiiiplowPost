1f88f4a84473b8ee0b2dec2564766f071338b485
YangZhiXiong <641484973@qq.com>
2023-09-10 19:44:12 +0800

    [前端]后台管理初始

--- chiiiplowpost-fe/src/router/index.js
+++ chiiiplowpost-fe/src/router/index.js
@@ -1,14 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import backLogin from '../back/login/backLogin.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'backLogin',
-      component: backLogin
-    }
+    // 后台登录
   ]
 })
 
